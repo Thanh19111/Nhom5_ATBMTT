52bf225cf3d09697996372e7320ee13ee387dac7
Thanh1911 <phantomcat191@gmail.com>
2024-12-14 16:28:56 +0700

    Chỉnh sửa nội dung báo cáo, thay thế thư viện mã hóa Cryptography thành Rijndael256 dễ sử dụng hơn

--- AES Encryption/Form1.Designer.cs
+++ AES Encryption/Form1.Designer.cs
@@ -29,27 +29,23 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.txtemess = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtekey = new System.Windows.Forms.TextBox();
             this.txtecipher = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.txteiv = new System.Windows.Forms.TextBox();
-            this.txtekey = new System.Windows.Forms.TextBox();
+            this.txtemess = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.txtdk = new System.Windows.Forms.TextBox();
-            this.txtdiv = new System.Windows.Forms.TextBox();
             this.txtdmess = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.txtdcipher = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -58,65 +54,40 @@
             // 
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.txtekey);
-            this.groupBox1.Controls.Add(this.txteiv);
             this.groupBox1.Controls.Add(this.txtecipher);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.txtemess);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(578, 543);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(580, 482);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Encryption";
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 74);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 22);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Message";
+            this.button1.Location = new System.Drawing.Point(233, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 40);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label2
+            // txtekey
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 193);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 22);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "IV";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 245);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 22);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Key";
-            // 
-            // txtemess
-            // 
-            this.txtemess.Location = new System.Drawing.Point(133, 31);
-            this.txtemess.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtemess.Multiline = true;
-            this.txtemess.Name = "txtemess";
-            this.txtemess.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtemess.Size = new System.Drawing.Size(350, 118);
-            this.txtemess.TabIndex = 3;
+            this.txtekey.Location = new System.Drawing.Point(133, 193);
+            this.txtekey.Name = "txtekey";
+            this.txtekey.Size = new System.Drawing.Size(350, 30);
+            this.txtekey.TabIndex = 7;
             // 
             // txtecipher
             // 
-            this.txtecipher.Location = new System.Drawing.Point(133, 299);
+            this.txtecipher.Location = new System.Drawing.Point(133, 255);
             this.txtecipher.Margin = new System.Windows.Forms.Padding(4);
             this.txtecipher.Multiline = true;
             this.txtecipher.Name = "txtecipher";
@@ -128,64 +99,81 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 342);
+            this.label4.Location = new System.Drawing.Point(16, 298);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(64, 22);
             this.label4.TabIndex = 4;
             this.label4.Text = "Cipher";
             // 
-            // txteiv
+            // txtemess
             // 
-            this.txteiv.Location = new System.Drawing.Point(133, 190);
-            this.txteiv.Name = "txteiv";
-            this.txteiv.Size = new System.Drawing.Size(350, 30);
-            this.txteiv.TabIndex = 6;
+            this.txtemess.Location = new System.Drawing.Point(133, 31);
+            this.txtemess.Margin = new System.Windows.Forms.Padding(4);
+            this.txtemess.Multiline = true;
+            this.txtemess.Name = "txtemess";
+            this.txtemess.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtemess.Size = new System.Drawing.Size(350, 118);
+            this.txtemess.TabIndex = 3;
             // 
-            // txtekey
+            // label3
             // 
-            this.txtekey.Location = new System.Drawing.Point(133, 237);
-            this.txtekey.Name = "txtekey";
-            this.txtekey.Size = new System.Drawing.Size(350, 30);
-            this.txtekey.TabIndex = 7;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 201);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Key";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Message";
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Controls.Add(this.txtdk);
-            this.groupBox2.Controls.Add(this.txtdiv);
             this.groupBox2.Controls.Add(this.txtdmess);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.txtdcipher);
             this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Controls.Add(this.label7);
             this.groupBox2.Controls.Add(this.label8);
             this.groupBox2.Location = new System.Drawing.Point(580, 0);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox2.Size = new System.Drawing.Size(564, 543);
+            this.groupBox2.Size = new System.Drawing.Size(564, 482);
             this.groupBox2.TabIndex = 8;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Encryption";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 401);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 40);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // txtdk
             // 
-            this.txtdk.Location = new System.Drawing.Point(133, 237);
+            this.txtdk.Location = new System.Drawing.Point(133, 193);
             this.txtdk.Name = "txtdk";
             this.txtdk.Size = new System.Drawing.Size(350, 30);
             this.txtdk.TabIndex = 7;
             // 
-            // txtdiv
-            // 
-            this.txtdiv.Location = new System.Drawing.Point(133, 190);
-            this.txtdiv.Name = "txtdiv";
-            this.txtdiv.Size = new System.Drawing.Size(350, 30);
-            this.txtdiv.TabIndex = 6;
-            // 
             // txtdmess
             // 
-            this.txtdmess.Location = new System.Drawing.Point(133, 299);
+            this.txtdmess.Location = new System.Drawing.Point(133, 255);
             this.txtdmess.Margin = new System.Windows.Forms.Padding(4);
             this.txtdmess.Multiline = true;
             this.txtdmess.Name = "txtdmess";
@@ -197,7 +185,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 342);
+            this.label5.Location = new System.Drawing.Point(8, 298);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(79, 22);
@@ -217,22 +205,12 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(13, 245);
+            this.label6.Location = new System.Drawing.Point(16, 201);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(42, 22);
             this.label6.TabIndex = 2;
             this.label6.Text = "Key";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(16, 193);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(29, 22);
-            this.label7.TabIndex = 1;
-            this.label7.Text = "IV";
             // 
             // label8
             // 
@@ -244,35 +222,15 @@
             this.label8.TabIndex = 0;
             this.label8.Text = "Cipher";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(229, 459);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 40);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(262, 459);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 40);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // AES_Encryption
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1157, 556);
+            this.ClientSize = new System.Drawing.Size(1145, 498);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AES_Encryption";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
@@ -288,21 +246,17 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox txtemess;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.TextBox txtecipher;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtekey;
-        private System.Windows.Forms.TextBox txteiv;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.TextBox txtdk;
-        private System.Windows.Forms.TextBox txtdiv;
         private System.Windows.Forms.TextBox txtdmess;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtdcipher;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.ComponentModel.BackgroundWorker backgroundWorker2;
         private System.Windows.Forms.Button button1;
--- AES Encryption/Form1.cs
+++ AES Encryption/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rijndael256;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +9,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Rijndael = Rijndael256.Rijndael;
 
 namespace AES_Encryption
 {
@@ -20,102 +22,27 @@ namespace AES_Encryption
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String mess = txtemess.Text;
-
-            Byte[] Key = GenerateRandomBytes(16);
-            Byte[] IV = GenerateRandomBytes(16);
-
-            String res = Encrypt(mess, Key, IV);
-
-            txteiv.Text = Convert.ToBase64String(IV);
-            txtekey.Text = Convert.ToBase64String(Key);
-            txtecipher.Text = res;
+            var inputText = txtemess.Text;
+            var key = txtekey.Text;
+            string EncryptText = Rijndael.Encrypt(inputText, key, KeySize.Aes256);
+            txtecipher.Text = EncryptText;
         }
-        public static byte[] HexToBytes(string hex)
-        {
-            if (hex.Length % 2 != 0)
-                throw new ArgumentException("Hex string must have an even length.");
-
-            byte[] bytes = new byte[hex.Length / 2];
-            for (int i = 0; i < hex.Length; i += 2)
-            {
-                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
-            }
-            return bytes;
-        }
+       
         
         private void button2_Click(object sender, EventArgs e)
         {
-            byte[] key = Convert.FromBase64String(txtdk.Text);
-            byte[] iv = Convert.FromBase64String(txtdiv.Text);
-    
-            String res = Decrypt(txtdcipher.Text, key, iv);
-            txtdmess.Text = res;
+            var inputText = txtdcipher.Text;
+            string key = txtdk.Text;
+            string DecryptText = Rijndael.Decrypt(inputText, key, KeySize.Aes256);
+            txtdmess.Text = DecryptText;
         }
 
-        public static byte[] GenerateRandomBytes(int length)
-        {
-            using (var rng = new RNGCryptoServiceProvider())
-            {
-                byte[] randomBytes = new byte[length];
-                rng.GetBytes(randomBytes);
-                return randomBytes;
-            }
-        }
+        
 
-        public static string Encrypt(string plaintext, byte[] key, byte[] iv)
-        {
-            if (key.Length != 16 && key.Length != 24 && key.Length != 32)
-            {
-                throw new CryptographicException("The specified key is not a valid size for this algorithm.");
-            }
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = key;
-                aesAlg.IV = iv;
-                aesAlg.Mode = CipherMode.CBC;
-                aesAlg.Padding = PaddingMode.PKCS7;
+        
 
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-                byte[] encrypted = encryptor.TransformFinalBlock(Encoding.UTF8.GetBytes(plaintext), 0, plaintext.Length);
+        
+        
 
-                return Convert.ToBase64String(encrypted);
-            }
-        }
-
-        public static string Decrypt(string ciphertext, byte[] key, byte[] iv)
-        {
-            if (key.Length != 16 && key.Length != 24 && key.Length != 32)
-            {
-                throw new CryptographicException("The specified key is not a valid size for this algorithm.");
-            }
-
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = key;
-                aesAlg.IV = iv;
-                aesAlg.Mode = CipherMode.CBC;
-                aesAlg.Padding = PaddingMode.PKCS7;
-
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-
-                try
-                {
-                    byte[] cipherBytes = Convert.FromBase64String(ciphertext);
-                    byte[] decrypted = decryptor.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
-                    return Encoding.UTF8.GetString(decrypted);
-                }
-                catch (FormatException)
-                {
-                    MessageBox.Show("Invalid Base64 string");
-                    return null;
-                }
-                catch (CryptographicException e)
-                {
-                    MessageBox.Show("Error during decryption: " + e.Message);
-                    return null;
-                }
-            }
-        }
     }
 }
